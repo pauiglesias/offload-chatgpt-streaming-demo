204aae5a5f1a2f52a4b4fe4f8350bf876a3f59dd
asimetrica <pauiglesias@gmail.com>
2023-04-10 08:45:31 +0200

    New config sample file

---
+++ config-sample.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Offload ChatGPT Streaming Demo
+ * Copy or rename this file to config.php and complete the following constants to start the demo.
+ */
+
+/**
+ * Your OpenAI API Key
+ * https://platform.openai.com/
+ */
+define('OPENAI_API_KEY', '[YOUR_API_KEY]');
+
+/**
+ * Your RapidAPI Api Key for Offload ChatGPT
+ * https://rapidapi.com/microdeploy/api/offload-chatgpt
+ */
+define('OFFLOAD_CHATGPT_API_KEY', '[YOUR_API_KEY]');
