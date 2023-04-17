416edb917e688b5836bc1774bbecb81e96b0ee47
asimetrica <pauiglesias@gmail.com>
2023-04-17 16:18:58 +0200

    Added the pantry id

--- config-sample.php
+++ config-sample.php
@@ -5,14 +5,23 @@
  * Copy or rename this file to config.php and complete the following constants to start the demo.
  */
 
+
 /**
  * Your OpenAI API Key
  * https://platform.openai.com/
  */
 define('OPENAI_API_KEY', '[YOUR_API_KEY]');
 
+
 /**
  * Your RapidAPI Api Key for Offload ChatGPT
  * https://rapidapi.com/microdeploy/api/offload-chatgpt
  */
 define('OFFLOAD_CHATGPT_API_KEY', '[YOUR_API_KEY]');
+
+
+/**
+ * Your PantryID for JSON storage
+ * https://getpantry.cloud/
+ */
+define('PantryID', '[YOUR_PANTRY_ID]');
